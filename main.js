3e7eb5b3bae764beadb89c9bad779914ff3b1eac
Anael Dahan <anael.assayag@gmail.com>
2017-12-19 13:14:02 +0200

    add title

--- main.js
+++ main.js
@@ -3,10 +3,25 @@ var reservations = {
   'Ted': { claimed: true }
 }
 
+console.log(reservations.Bob.claimed)
+
 var name = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
+var claimReservation = function (name) {
   // write your code here!
+  if (reservations[name] && ! reservations[name].claimed) {
+    alert('Welcome ' + name)
+  }
+  else if (reservations[name] && reservations[name].claimed) {
+    alert('your reservation has already been claimed')
+  }
+  else{
+    reservations[name] = {claimed: true}
+    alert('Your reservation has been considered')
+  }
+
 }
 
-claimReservation();
+claimReservation(name);
+console.log(reservations)
+
